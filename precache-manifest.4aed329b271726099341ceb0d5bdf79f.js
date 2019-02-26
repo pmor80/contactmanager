1048ebbaf41369e48e38f6b869658b9b0cdb621d
Pawel Morawski <pawel@wearegrid>
2019-02-26 16:50:16 +0000

    Updates

---
+++ precache-manifest.4aed329b271726099341ceb0d5bdf79f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90a5ae7c49d09dc27c53",
+    "url": "/github.io/contactmanager/static/js/runtime~main.90a5ae7c.js"
+  },
+  {
+    "revision": "d07c98ff89c7df607547",
+    "url": "/github.io/contactmanager/static/js/main.d07c98ff.chunk.js"
+  },
+  {
+    "revision": "5ddfbb8fbda75d4e297c",
+    "url": "/github.io/contactmanager/static/js/1.5ddfbb8f.chunk.js"
+  },
+  {
+    "revision": "d07c98ff89c7df607547",
+    "url": "/github.io/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "5ddfbb8fbda75d4e297c",
+    "url": "/github.io/contactmanager/static/css/1.2425e64b.chunk.css"
+  },
+  {
+    "revision": "89e34aa95849e81a56d761a627bdc9af",
+    "url": "/github.io/contactmanager/index.html"
+  }
+];
